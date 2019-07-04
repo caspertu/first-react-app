0906032594ea67d97700d068e4b8a4767dfd2301
caspertu <caspertu@gmail.com>
2019-07-04 15:40:40 +0800

    init

--- README.md
+++ README.md
@@ -1,9 +1,19 @@
 # FIRST REACT APP
 
+## 初始化
+
+```bash
+yarn install
+```
+
 ## 可用的脚本
 
 在项目目录，你可以运行
 
-### `npm start`
+### `yarn start`
 
 在开发模式启动应用
+
+## History
+
+2019-07-04 函数式编程
--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import ClickCounter from './ClickCounter';
+import ClickCounter from './components/ClickCounter';
 import './App.css';
 
 class App extends Component {
